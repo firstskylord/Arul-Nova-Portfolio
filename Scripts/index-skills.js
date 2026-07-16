668314f72bb72978d61293a2ccc00fe7952f57c6
firstskylord <gamingajitesh9@gmail.com>
2026-07-16 22:39:38 +0530

    Implement tabbed navigation for skills and education sections; update styles and structure in index.html and index.css

---
+++ Scripts/index-skills.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".tab-content");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    contents.forEach((tab) => tab.classList.remove("active"));
+
+    button.classList.add("active");
+    document.getElementById(button.dataset.tab).classList.add("active");
+  });
+});
